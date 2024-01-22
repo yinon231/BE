d186395fcf35ca09d80e6cf7365c5256bd88dda0
yinonmaman2 <yinon231@gmail.com>
2024-01-22 18:41:03 +0200

    add morgan

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const http=require('http');
-const {router}=require('./router');
-const server=http.createServer((req,res)=>{
- router(req,res);
-   
-   
+const http = require('http');
+const morgan = require('morgan');
+const { router } = require('./router');
+
+const server = http.createServer((req, res) => {
+  // Use Morgan middleware for logging HTTP requests
+  morgan('combined')(req, res, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // Your router logic
+    router(req, res);
+  });
 });
-server.listen(3000);
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
--- repository.js
+++ repository.js
@@ -1,110 +1,108 @@
-const fs=require('fs');
- getAllDonations=()=> {
+const fs = require('fs');
+getAllDonations = () => {
     let data = '';
     return new Promise((resolve, reject) => {
         fs.createReadStream('donations.json', 'utf8')
-        .on('error', error => {
-            reject(error);
-        })
-        .on('data', chunk => {
-            data += chunk;
-        })
-        .on('end', () => {
-            resolve(data);
-        });
+            .on('error', error => {
+                reject(error);
+            })
+            .on('data', chunk => {
+                data += chunk;
+            })
+            .on('end', () => {
+                resolve(data);
+            });
     });
 }
 getAllDonationsById = (queryId) => {
     let data = '';
     return new Promise((resolve, reject) => {
         fs.createReadStream('donations.json', 'utf8')
-        .on('error', error => {
-            reject(error);
-        })
-        .on('data', chunk => {
-            data += chunk;
-        })
-        .on('end', () => {
-            try {
-                data = JSON.parse(data);
-                let filteredData = data.find(item => item.id == queryId);
-                filteredData=JSON.stringify(filteredData);
-                resolve(filteredData);
-            } catch (parseError) {
-                reject(parseError);
-            }
-        });
+            .on('error', error => {
+                reject(error);
+            })
+            .on('data', chunk => {
+                data += chunk;
+            })
+            .on('end', () => {
+                try {
+                    data = JSON.parse(data);
+                    let filteredData = data.find(item => item.id == queryId);
+                    filteredData = JSON.stringify(filteredData);
+                    resolve(filteredData);
+                } catch (parseError) {
+                    reject(parseError);
+                }
+            });
     });
-  
+
 }
-    
-createDonation=async(value)=>{
-    let data=await getAllDonations();
-    data=JSON.parse(data);
+
+createDonation = async (value) => {
+    let data = await getAllDonations();
+    data = JSON.parse(data);
     let id = data.length + 1;
-    data.push({ id, ...value});
+    data.push({ id, ...value });
     return new Promise((resolve, reject) => {
-        const writeStream=fs.createWriteStream('donations.json', 'utf8')
+        const writeStream = fs.createWriteStream('donations.json', 'utf8')
         writeStream.on('error', error => {
             reject(error);
         });
-        data=JSON.stringify(data);
+        data = JSON.stringify(data);
         writeStream.write(data);
         writeStream.end();
         writeStream.on('finish', () => {
             resolve(id);
         });
-    }); 
-}
-deleteDonation=async(value)=>{
-    let data=await getAllDonations();
-    data=JSON.parse(data);
-    const {id} = value;
-    data=data.filter((item) => {
-        return item.id !== id;
     });
+}
+deleteDonation = async (value) => {
+    let data = await getAllDonations();
     return new Promise((resolve, reject) => {
-        if(!data.find(item=>item.id===id)) 
-        {
-             reject(new Error("ID not found"));
+        data = JSON.parse(data);
+        const { id } = value;
+        if (!data.find(item => item.id === id)) {
+            reject(new Error("ID not found"));
         }
-        const writeStream=fs.createWriteStream('donations.json', 'utf8')
+        data = data.filter((item) => {
+            return item.id !== id;
+        });
+        const writeStream = fs.createWriteStream('donations.json', 'utf8')
         writeStream.on('error', error => {
             reject(error);
         });
-        data=JSON.stringify(data);
+        data = JSON.stringify(data);
         writeStream.write(data);
         writeStream.end();
         writeStream.on('finish', () => {
             resolve(true);
         });
-    }); 
-}
-updateDonation=async(value)=>{
-    let data=await getAllDonations();
-    data=JSON.parse(data);
-    const {id,items} = value;
-    data=data.map((item) => {
-        if (item.id === id) {
-            return { id, items };
-        }
-        return item;
     });
+}
+updateDonation = async (value) => {
+    let data = await getAllDonations();
     return new Promise((resolve, reject) => {
-        if(!data.find(item=>item.id===id)) 
-        {
-             reject(new Error("ID not found"));
+        data = JSON.parse(data);
+        const { id, items } = value;
+        if (!data.find(item => item.id === id)) {
+            reject(new Error("ID not found"));
         }
-        const writeStream=fs.createWriteStream('donations.json', 'utf8')
+        data = data.map((item) => {
+            if (item.id === id) {
+                return { id, items };
+            }
+            return item;
+        });
+        const writeStream = fs.createWriteStream('donations.json', 'utf8')
         writeStream.on('error', error => {
             reject(error);
         });
-        data=JSON.stringify(data);
+        data = JSON.stringify(data);
         writeStream.write(data);
         writeStream.end();
         writeStream.on('finish', () => {
             resolve(true);
         });
-    }); 
+    });
 }
-module.exports={getAllDonations,getAllDonationsById,createDonation,deleteDonation,updateDonation};
+module.exports = { getAllDonations, getAllDonationsById, createDonation, deleteDonation, updateDonation };
