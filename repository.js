15a413a0058eacea17a19c4aa54bce6e79ffd82a
yinonmaman2 <yinon231@gmail.com>
2024-02-06 10:40:12 +0200

    .

--- repository.js
+++ repository.js
@@ -1,100 +1,104 @@
-const fs = require('fs');
-getAllDonations = () => {
-    let data = '';
-    return new Promise((resolve, reject) => {
-        fs.createReadStream('donations.json', 'utf8')
-            .on('error', error => {
-                reject(error);
-            })
-            .on('data', chunk => {
-                data += chunk;
-            })
-            .on('end', () => {
-                resolve(data);
-            });
+const fs = require("fs");
+const getAllDonations = () => {
+  let data = "";
+  return new Promise((resolve, reject) => {
+    fs.createReadStream("donations.json", "utf8")
+      .on("error", (error) => {
+        reject(error);
+      })
+      .on("data", (chunk) => {
+        data += chunk;
+      })
+      .on("end", () => {
+        resolve(data);
+      });
+  });
+};
+const getAllDonationsById = async (queryId) => {
+  let data = await getAllDonations();
+  return new Promise((resolve, reject) => {
+    try {
+      data = JSON.parse(data);
+      let filteredData = data.find((item) => item.id == queryId);
+      filteredData = JSON.stringify(filteredData);
+      resolve(filteredData);
+    } catch (parseError) {
+      reject(parseError);
+    }
+  });
+};
+
+const createDonation = async (value) => {
+  let data = await getAllDonations();
+  data = JSON.parse(data);
+  let id = data.length + 1;
+  data.push({ id, ...value });
+  return new Promise((resolve, reject) => {
+    const writeStream = fs.createWriteStream("donations.json", "utf8");
+    writeStream.on("error", (error) => {
+      reject(error);
     });
-}
-getAllDonationsById = async (queryId) => {
-    let data = await getAllDonations();
-    return new Promise((resolve, reject) => {
-        try {
-            data = JSON.parse(data);
-            let filteredData = data.find(item => item.id == queryId);
-            filteredData = JSON.stringify(filteredData);
-            resolve(filteredData);
-
-        } catch (parseError) {
-            reject(parseError);
-        }
+    data = JSON.stringify(data);
+    writeStream.write(data);
+    writeStream.end();
+    writeStream.on("finish", () => {
+      resolve(id);
     });
-
-}
-
-createDonation = async (value) => {
-    let data = await getAllDonations();
+  });
+};
+const deleteDonation = async (value) => {
+  let data = await getAllDonations();
+  return new Promise((resolve, reject) => {
     data = JSON.parse(data);
-    let id = data.length + 1;
-    data.push({ id, ...value });
-    return new Promise((resolve, reject) => {
-        const writeStream = fs.createWriteStream('donations.json', 'utf8')
-        writeStream.on('error', error => {
-            reject(error);
-        });
-        data = JSON.stringify(data);
-        writeStream.write(data);
-        writeStream.end();
-        writeStream.on('finish', () => {
-            resolve(id);
-        });
+    const { id } = value;
+    if (!data.find((item) => item.id === id)) {
+      reject(new Error("ID not found"));
+    }
+    data = data.filter((item) => {
+      return item.id !== id;
     });
-}
-deleteDonation = async (value) => {
-    let data = await getAllDonations();
-    return new Promise((resolve, reject) => {
-        data = JSON.parse(data);
-        const { id } = value;
-        if (!data.find(item => item.id === id)) {
-            reject(new Error("ID not found"));
-        }
-        data = data.filter((item) => {
-            return item.id !== id;
-        });
-        const writeStream = fs.createWriteStream('donations.json', 'utf8')
-        writeStream.on('error', error => {
-            reject(error);
-        });
-        data = JSON.stringify(data);
-        writeStream.write(data);
-        writeStream.end();
-        writeStream.on('finish', () => {
-            resolve(true);
-        });
+    const writeStream = fs.createWriteStream("donations.json", "utf8");
+    writeStream.on("error", (error) => {
+      reject(error);
     });
-}
-updateDonation = async (value) => {
-    let data = await getAllDonations();
-    return new Promise((resolve, reject) => {
-        data = JSON.parse(data);
-        const { id, items } = value;
-        if (!data.find(item => item.id === id)) {
-            reject(new Error("ID not found"));
-        }
-        data = data.map((item) => {
-            if (item.id === id) {
-                return { id, items };
-            }
-            return item;
-        });
-        const writeStream = fs.createWriteStream('donations.json', 'utf8')
-        writeStream.on('error', error => {
-            reject(error);
-        });
-        data = JSON.stringify(data);
-        writeStream.write(data);
-        writeStream.end();
-        writeStream.on('finish', () => {
-            resolve(true);
-        });
+    data = JSON.stringify(data);
+    writeStream.write(data);
+    writeStream.end();
+    writeStream.on("finish", () => {
+      resolve(true);
     });
-}
-module.exports = { getAllDonations, getAllDonationsById, createDonation, deleteDonation, updateDonation };
+  });
+};
+const updateDonation = async (value) => {
+  let data = await getAllDonations();
+  return new Promise((resolve, reject) => {
+    data = JSON.parse(data);
+    const { id, items } = value;
+    if (!data.find((item) => item.id === id)) {
+      reject(new Error("ID not found"));
+    }
+    data = data.map((item) => {
+      if (item.id === id) {
+        return { id, items };
+      }
+      return item;
+    });
+    const writeStream = fs.createWriteStream("donations.json", "utf8");
+    writeStream.on("error", (error) => {
+      reject(error);
+    });
+    data = JSON.stringify(data);
+    writeStream.write(data);
+    writeStream.end();
+    writeStream.on("finish", () => {
+      resolve(true);
+    });
+  });
+};
+module.exports = {
+  getAllDonations,
+  getAllDonationsById,
+  createDonation,
+  deleteDonation,
+  updateDonation,
+};
